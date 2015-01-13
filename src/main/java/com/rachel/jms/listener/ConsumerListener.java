061b2491153df013d584c0935afc8889614d3ae9
Rachel Mills <rachel.h.mills@gmail.com>
2015-01-13 22:21:23 +1100

    Test

--- src/main/java/com/rachel/jms/listener/ConsumerListener.java
+++ src/main/java/com/rachel/jms/listener/ConsumerListener.java
@@ -7,7 +7,8 @@ public class ConsumerListener implements MessageListener {
 
 	@Override
 	public void onMessage(Message arg0) {
-		System.out.println("In onMessage ... ");	
+		System.out.println("In onMessage ... ");
+		System.out.println("Testing");
 	}
 
 }
